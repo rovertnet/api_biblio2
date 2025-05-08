e10418dbc0b036ad3aede93046c4fe3d828451b3
rovertnet <matundukabamba@gmail.com>
2025-05-09 00:05:18 +0100

    last commit

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,13 +1,14 @@
 // === src/books/books.module.ts ===
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Book } from './book.entity';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
+import { Book } from './book.entity';
+import { SubscriptionsModule } from '../subscriptions/subscriptions.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Book])],
-  providers: [BooksService],
+  imports: [TypeOrmModule.forFeature([Book]), SubscriptionsModule],
   controllers: [BooksController],
+  providers: [BooksService],
 })
 export class BooksModule {}
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -9,7 +9,38 @@ export class BooksService {
   constructor(
     @InjectRepository(Book)
     private bookRepo: Repository<Book>,
-  ) {}
+  ) {}// === src/books/books.service.ts ===
+  import { Injectable, NotFoundException } from '@nestjs/common';
+  import { InjectRepository } from '@nestjs/typeorm';
+  import { Repository } from 'typeorm';
+  import { Book } from './book.entity';
+  
+  @Injectable()
+  export class BooksService {
+    constructor(
+      @InjectRepository(Book)
+      private readonly bookRepo: Repository<Book>,
+    ) {}
+  
+    async read(id: number) {
+      const book = await this.bookRepo.findOneBy({ id });
+      if (!book) {
+        throw new NotFoundException('Livre non trouvé');
+      }
+      return book;
+    }
+  
+    async download(id: number) {
+      const book = await this.bookRepo.findOneBy({ id });
+      if (!book) {
+        throw new NotFoundException('Livre non trouvé');
+      }
+  
+      // Simule le téléchargement du livre
+      return { message: `Livre "${book.title}" téléchargé.` };
+    }
+  }
+  
 
   findAll() {
     return this.bookRepo.find();
