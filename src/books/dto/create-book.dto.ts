b74dcabd9b459abb80018ef9c00eb48f6e2b9726
rovertnet <matundukabamba@gmail.com>
2025-05-11 23:13:45 +0100

    last commit

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,9 +1,19 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString, IsOptional, IsInt } from 'class-validator';
 
 export class CreateBookDto {
+  @IsString()
   @IsNotEmpty()
   title: string;
 
+  @IsString()
   @IsNotEmpty()
   author: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  categoryId: number;
 }
