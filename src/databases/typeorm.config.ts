99bbcdc23c2453de5be21489d5f0b7798f40f69c
rovertnet <matundukabamba@gmail.com>
2025-05-11 18:22:31 +0100

    last commit

--- src/databases/typeorm.config.ts
+++ src/databases/typeorm.config.ts
@@ -1,16 +1,17 @@
-// === src/database/typeorm.config.ts ===
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { User } from '../users/user.entity';
 import { Book } from '../books/book.entity';
+import { Category } from '../categories/category.entity';
 import { Subscription } from '../subscriptions/subscription.entity';
+import { ReadingHistory } from '../reading/reading-history.entity';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
-  port: +(process.env.DB_PORT ?? 3306),
-  username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'bibliotheque',
-  entities: [User, Book, Subscription],
-  synchronize: true,
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: '',
+  database: 'bibliotheque',
+  entities: [User, Book, Category, Subscription, ReadingHistory],
+  synchronize: true, // à activer uniquement en dev
 };
