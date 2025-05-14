e59e9810d56a98572fd98b65ff5f21b7fddd8638
rovertnet <matundukabamba@gmail.com>
2025-05-14 23:04:27 +0100

    another commit

--- src/subscriptions/subscriptions.module.ts
+++ src/subscriptions/subscriptions.module.ts
@@ -3,11 +3,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Subscription } from './subscription.entity';
 import { SubscriptionsService } from './subscriptions.service';
+import { SubscriptionsController } from './subscriptions.controller'; // ✅ Ajout ici
 import { User } from '../users/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Subscription, User])],
   providers: [SubscriptionsService],
+  controllers: [SubscriptionsController], // ✅ Enregistrement du contrôleur
   exports: [SubscriptionsService],
 })
 export class SubscriptionsModule {}
